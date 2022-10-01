9c7bcb79c139509aa4db28c4b3548a18266e8354
Che Wei (Jacky) Lee <aboutjax1990@gmail.com>
2022-10-01 09:49:49 -0400

    update

--- pages/_app.js
+++ pages/_app.js
@@ -6,10 +6,7 @@ import Script from "next/script";
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-QQ7B8F4CHY"
-        strategy="afterInteractive"
-      />
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-QQ7B8F4CHY" />
       <Script id="google-analytics" strategy="afterInteractive">
         {`
           window.dataLayer = window.dataLayer || [];
