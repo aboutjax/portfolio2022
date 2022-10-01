fafda85c2b9c5c8ac0e731524eb747790497d7f0
Che Wei (Jacky) Lee <aboutjax1990@gmail.com>
2022-10-01 09:45:45 -0400

    add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,25 @@
 import React from "react";
 import "../styles/globals.css";
 import Layout from "../components/layout";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-QQ7B8F4CHY"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-QQ7B8F4CHY');
+        `}
+      </Script>
+
       <Component {...pageProps} />
     </Layout>
   );
